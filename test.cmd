c0eefb90d582a27987a44b2c5acc8c92e78b3cb5
Эдуард Захаров <ezaharov@vtb.ru>
2023-11-14 23:22:53 +0300

    delete not need boto files

--- test.cmd
+++ test.cmd
@@ -5,7 +5,7 @@ set s3lo=python -m s3lo
 if exist s3lo.pyz set s3lo=python s3lo.pyz
 
 %s3lo% list
-::%s3lo% upload -f readme.md
-::%s3lo% get -k readme.md
+%s3lo% upload -f readme.md
+%s3lo% get -k readme.md
 
 	
